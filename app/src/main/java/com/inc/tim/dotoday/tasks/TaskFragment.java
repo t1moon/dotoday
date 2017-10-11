8b964eefd3496995164a0ff7db7a3ee72c6f671c
t1moon <timurchik1024@gmail.com>
2017-10-11 21:46:30 +0300

    trying do animation

--- app/src/main/java/com/inc/tim/dotoday/tasks/TaskFragment.java
+++ app/src/main/java/com/inc/tim/dotoday/tasks/TaskFragment.java
@@ -1,5 +1,7 @@
 package com.inc.tim.dotoday.tasks;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -8,6 +10,7 @@ import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.animation.ValueAnimatorCompat;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -40,6 +43,7 @@ import java.util.List;
 
 import static com.inc.tim.dotoday.R.id.spinner_nav;
 import static com.inc.tim.dotoday.util.CommonUtils.ColorUtil.MATERIAL_COLORS;
+import static com.inc.tim.dotoday.util.CommonUtils.ColorUtil.MATERIAL_COLORS_LIGHT;
 import static com.inc.tim.dotoday.util.CommonUtils.ColorUtil.STATUSBAR_MATERIAL_COLORS;
 
 public class TaskFragment extends Fragment implements TasksContract.View,
@@ -201,10 +205,43 @@ public class TaskFragment extends Fragment implements TasksContract.View,
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 AppBarLayout appBarLayout = (AppBarLayout) getActivity().findViewById(R.id.appbar_layout);
+                Integer colorFrom = MATERIAL_COLORS[0];
+                Integer colorTo = MATERIAL_COLORS[1];
+                Integer colorStatusFrom = STATUSBAR_MATERIAL_COLORS[2];
+                Integer colorStatusTo = STATUSBAR_MATERIAL_COLORS[3];
+                ValueAnimator colorAnimator = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
+                ValueAnimator colorStatusAnimator = ValueAnimator.ofObject(new ArgbEvaluator(), colorStatusFrom, colorStatusTo);
+
+                final Toolbar toolbar = (Toolbar) getActivity().findViewById(R.id.toolbar);
+
+                colorAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        toolbar.setBackgroundColor((Integer) animation.getAnimatedValue());
+                    }
+                });
+                colorStatusAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                            getActivity().getWindow().setStatusBarColor((Integer) animation.getAnimatedValue());
+                        }
+//                        toolbar.setBackgroundColor((Integer) animation.getAnimatedValue());
+                    }
+                });
 
                 ((TasksActivity)getActivity()).setCreatedCategory(position);
-                ToolbarUtils.setToolbarColor((AppCompatActivity) getActivity(),
-                        ((TasksActivity) getActivity()).getSupportActionBar(), appBarLayout);
+                colorAnimator.setDuration(800);
+                colorAnimator.setStartDelay(0);
+                colorAnimator.start();
+                colorStatusAnimator.setDuration(800);
+                colorStatusAnimator.setStartDelay(0);
+                colorStatusAnimator.start();
+                ColorDrawable toolbarColor = new ColorDrawable(MATERIAL_COLORS[category]);
+                spinner.setPopupBackgroundDrawable(toolbarColor);
+//                ToolbarUtils.setToolbarColor((AppCompatActivity) getActivity(),
+//                        ((TasksActivity) getActivity()).getSupportActionBar(), appBarLayout);
+
 
                 presenter.loadCategoryTasks(position);
 
